38ffd8701e5d1f2916391aed1649e6a5ab624656
pavananumoju <pavananumoju@gmail.com>
2021-09-11 19:56:04 +0530

    handling radio buttons

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
   @ViewChild('f') signUpForm: NgForm;
   defaultSelectVal = 'pet';
   answer = ''
+  genders = ['male', 'female'];
 
   suggestUserName() {
     const suggestedName = 'Superuser';
