36b14e55afcdf7c311197f7ec9b75ea43d86a269
pavananumoju <pavananumoju@gmail.com>
2021-09-11 19:39:44 +0530

    ngModel bindings in Form

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -9,6 +9,7 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
   @ViewChild('f') signUpForm: NgForm;
   defaultSelectVal = 'pet';
+  answer = ''
 
   suggestUserName() {
     const suggestedName = 'Superuser';
