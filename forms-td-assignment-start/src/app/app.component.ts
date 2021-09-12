0409c18d55d54b289ad7b8695b69675f037a6a0b
pavananumoju <pavananumoju@gmail.com>
2021-09-12 17:09:54 +0530

    forms assignment finish

--- forms-td-assignment-start/src/app/app.component.ts
+++ forms-td-assignment-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') formObj : NgForm;
+  levelSelect = ['Basic', 'Advanced', 'Pro'];
+  defaultSelectVal = 'Basic';
+  userData = {
+    email : '',
+    level: ''
+  }
+
+  onSubmit(){
+    console.info(this.formObj.value);
+    this.userData.email = this.formObj.value.email;
+    this.userData.level = this.formObj.value.level;
+  }
 
 }
