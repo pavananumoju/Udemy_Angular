f7249f3a63c9697b248fc576f6577b07e7f11bf5
pavananumoju <pavananumoju@gmail.com>
2021-09-12 23:58:29 +0530

    reactive forms assignment finish

--- forms-reactive-assignment-start/src/app/app.component.ts
+++ forms-reactive-assignment-start/src/app/app.component.ts
@@ -1,9 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  projectStatuses = ['Stable', 'Critical', 'Finished'];
+  projectForm : FormGroup;
+
+  ngOnInit(){ 
+    this.projectForm = new FormGroup({
+      'projectName' : new FormControl(null, [Validators.required, this.forbiddenProjectNameSync.bind(this)], this.forbiddenProjectNameAsync.bind(this)),
+      'projectEmail' : new FormControl(null, [Validators.required, Validators.email]),
+      'projectStatus' : new FormControl(['Critical'])
+    });
+  }
+
+  forbiddenProjectNameSync(control: FormControl): {[s:string]:boolean}{
+    if(control.value === 'Test Sync'){
+      return {'forbiddenNameSync':true};
+    } else{
+      return null;
+    }
+  }
+
+  forbiddenProjectNameAsync(control: FormControl): Promise<any> | Observable<any>{
+    const promise = new Promise((resolve, reject)=>{
+      setTimeout(() => {
+        if(control.value === 'Test Async'){
+          resolve ({'forbiddenNameAsync':true});
+        } else{
+          resolve (null);
+        }
+      }, 1500);
+    });
+    return promise;
+  }
+
+  onSubmit(){
+    console.info(this.projectForm);
+  }
+
 }
